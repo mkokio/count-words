2a9e046e3a1cd5b9853c88725acc12f7e5688a3e
mkokio <108322460+mkokio@users.noreply.github.com>
2022-06-28 15:18:51 +0900

    Add files via upload

---
+++ analyzing_text_count_method.py
@@ -0,0 +1,23 @@
+def count_words(book):
+    """Prompts the user for a keyword and returns the occurance count."""
+    try:
+        with open(book, encoding='utf-8') as file_object:
+            contents = file_object.read().lower()
+            
+            while True:
+                word_lookup = input("What word would you like counted? 'r' to restart. ").lower()
+                if word_lookup == 'r':
+                    print("\tProgram restarted.")
+                    break
+                print(f"\tThe word '{word_lookup}' appears {contents.split().count(word_lookup)} times in file '{book}'.")
+    except FileNotFoundError:
+        print(f"File ({book})not found.")   #Write 'pass' to silently ignores the error
+
+print("For example: 'alice' 'bible' 'lotr'\n")
+while True:
+    book_title = input("What's the title of your book? 'q' to quit: ")
+    if book_title == 'q':
+        print("\tProgram ended.")
+        break
+    else:
+        count_words(f'text_files/{book_title}.txt')
---
+++ analyzing_text_for_loop.py
@@ -0,0 +1,27 @@
+def count_words(book):
+    """Prompts the user for a keyword and returns the occurance count."""
+    try:
+        with open(book, encoding='utf-8') as file_object:
+            contents = file_object.read()
+            while True:
+                x = 0
+                word_lookup = input("What word would you like counted? 'r' to restart. ")
+                if word_lookup == 'r':
+                    print("\tProgram restarted.")
+                    break
+                else:
+                    for word in contents.split(): #this splits each word into an item on in a list
+                        if word.lower() == word_lookup.lower(): #this makes the search case insensitive
+                            x += 1 #adds to the counter
+                print(f"\tThe word '{word_lookup}' appears {x} times in file '{book}'.")
+    except FileNotFoundError:
+        print(f"\nFile ({book})not found.") #Write 'pass' to silently ignores the error
+
+print("For example: 'alice' 'bible' 'lotr'\n") #
+while True:
+    book_title = input("What's the title of your book? 'q' to quit: ")
+    if book_title == 'q':
+        print("\tProgram ended.")
+        break
+    else:
+        count_words(f'text_files/{book_title}.txt')
